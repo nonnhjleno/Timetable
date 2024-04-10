7979a716c60140611e3722f0fd730cd4377b89eb
nonnhjleno <taaaisei999@gmail.com>
2024-04-10 23:24:30 +0900

    セミコロン追加しただけ

--- src/routes/Timetable.tsx
+++ src/routes/Timetable.tsx
@@ -5,7 +5,8 @@ const Timetable = () => {
   const { semesterId } = useParams<{ semesterId?: string }>();
 
   const id = semesterId ? parseInt(semesterId) : 1;
-  const label = `${Math.ceil(id / 2)}年${id % 2 === 1 ? '前期' : '後期'}`
+  const label = `${Math.ceil(id / 2)}年${id % 2 === 1 ? '前期' : '後期'}`;
+  
   return (
     <div>{label}</div>
   )
